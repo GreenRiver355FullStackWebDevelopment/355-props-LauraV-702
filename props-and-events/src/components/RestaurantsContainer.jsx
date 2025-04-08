963439bd416439937bcac9d5770e540443a0834d
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-04-08 11:48:38 -0700

    Added comments for RestaurantsContainer function

--- props-and-events/src/components/RestaurantsContainer.jsx
+++ props-and-events/src/components/RestaurantsContainer.jsx
@@ -6,10 +6,12 @@ import Restaurant from "./Restaurant";
 function RestaurantsContainer() {
   return (
     <div className="restaurantContainer">
+      {/* iterates through each element of restaurant and renders through to app */}
       {restaurants.map(restaurant => <Restaurant restaurant={restaurant} />)}
     </div>
   );
 }
 
+//Export info out to be used
 export default RestaurantsContainer;
 
